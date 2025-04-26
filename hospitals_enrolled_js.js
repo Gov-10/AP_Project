7dd57ce534f92efb92a8d260fcea72e22ebcbbb6
Govind <govindsys1008@gmail.com>
2025-04-27 00:24:04 +0530

    added ham to the specified pages

--- hospitals_enrolled_js.js
+++ hospitals_enrolled_js.js
@@ -49,3 +49,10 @@ const hospitalData = {
       });
     }
   }
+  const hamMenu = document.querySelector(".ham-menu");
+  const offScreenMenu = document.querySelector(".off-screen-menu");
+
+  hamMenu.addEventListener("click", () => {
+    offScreenMenu.classList.toggle("active");
+    hamMenu.classList.toggle("active");
+  });
